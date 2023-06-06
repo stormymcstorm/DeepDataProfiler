2d4a525fc059ecdf8cc2403a866cd2d2a9b94a67
Carson Storm <carson.storm@outlook.com>
2023-06-06 15:37:00 -0700

    fix model load

--- papers_with_code/ExperimentalObservations/AAAI-code-mapper/cifar_extract.py
+++ papers_with_code/ExperimentalObservations/AAAI-code-mapper/cifar_extract.py
@@ -20,12 +20,7 @@ def unpickle(file):
 
 def load_model(path, num_classes):
     model = ResNet18(num_classes)
-    state = torch.load(path)
-    try:
-        model.load_state_dict(state)
-    except:
-        print(state)
-        raise
+    model.load_state_dict(torch.load(path))
     # model.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage)['model_state_dict'])
     return model
 
