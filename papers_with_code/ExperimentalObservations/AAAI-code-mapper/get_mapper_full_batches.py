e34771e1f42cc08b2b7306820fa5cef3fa23638a
Carson Storm <carson.storm@outlook.com>
2023-06-07 16:32:38 -0700

    working on full activations

--- papers_with_code/ExperimentalObservations/AAAI-code-mapper/get_mapper_full_batches.py
+++ papers_with_code/ExperimentalObservations/AAAI-code-mapper/get_mapper_full_batches.py
@@ -4,31 +4,75 @@
 Created on Mon Jul 12 02:41:48 2021
 
 """
+
+import argparse
 import os
 import sys
+import pathlib
+
 import numpy as np
 import pandas as pd
 import h5py
 import torch
 import torch.nn.functional as F
+
 from mapper_interactive.mapper_CLI import get_mapper_graph
 from get_knn import elbow_eps
 
 def read_activation(filepath, layer):
     with h5py.File(filepath, 'r') as f:
-        activation = f[layer][:]
+        try:
+            activation = f[layer][:]
+        except:
+            raise ValueError(f'Unrecognized layer {layer}. Please choose from: {list(f.keys())}')
         return activation
 
 if __name__ == '__main__':
-    os.environ['KMP_DUPLICATE_LIB_OK']='True'
-    layer = sys.argv[1]
-    interval = int(sys.argv[2])
-    overlap = int(sys.argv[3])
-    
-    DATASET = 'CIFAR10'
-    num_classes = 10
+    parser = argparse.ArgumentParser(
+        description='Get mapper for full activation vectors',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'layer', type=str,
+        help='The layer to extract activations for'
+    )
+    parser.add_argument(
+        '--interval', type=int, default=10,
+        help='Number of hypercubes along each dimension. Sometimes referred to as resolution.'
+    )
+    parser.add_argument(
+        '--overlap', type=int, default=50,
+        help='TODO'
+    )
+    parser.add_argument(
+        '--dataset', choices=['CIFAR10'], type=str.upper, default='CIFAR10',
+        help='The dataset to train ResNet18 on.'
+    )
+    parser.add_argument(
+        '--act-dir', type=pathlib.Path, default='activations',
+        help='The directory to write the activations to.'
+    )
+    parser.add_argument(
+        '--graph-dir', type=pathlib.Path, default='mapper_graphs',
+        help='The directory to write the mapper graphs to.'
+    )
 
-    activation_dir = f'../activations/{DATASET.lower()}/resnet18_custom_aug/full_activations'
+    args = parser.parse_args()
+
+    os.environ['KMP_DUPLICATE_LIB_OK']='True'
+    layer = args.layer
+    interval = args.interval
+    overlap = args.overlap
+    
+    DATASET = args.dataset
+
+    if DATASET == 'CIFAR10':
+        num_classes = 10
+    else:
+        raise NotImplementedError(f'Sorry this script does not support {DATASET}')
+
+    activation_dir = args.act_dir / f'{DATASET}_ResNet18_Custom_Aug' / 'full_activations'
+    graph_dir = args.graph_dir / 'full_batches'
     
     names = ['airplane',
       'automobile',
@@ -56,8 +100,8 @@ if __name__ == '__main__':
         layer_activations_i = np.vstack([l for l in layer_activations_i_new])
         label_i = np.repeat(i, len(layer_activations_i)).reshape(-1,1)
         layer_activations_i = np.hstack([label_i, layer_activations_i])
-        layer_activations.append(layer_activations_i)
-    
+        layer_activations.append(layer_activations_i)    
+
     layer_activations = np.vstack([layer_activations_i for layer_activations_i in layer_activations])
     layer_activations_df = pd.DataFrame(layer_activations)
     
@@ -82,10 +126,13 @@ if __name__ == '__main__':
             
     min_samples = 5
     
-    output_dir = '../mapper_graphs/full_batches/'
-    output_fname = 'full_batches_'+layer
-    
-    get_mapper_graph(layer_activations_df, interval, overlap, eps, min_samples, output_dir, output_fname, is_parallel=False)
-    
-    
-
+    get_mapper_graph(
+        df=layer_activations_df, 
+        interval=interval, 
+        overlap=overlap, 
+        eps=eps, 
+        min_samples=min_samples, 
+        output_dir=str(graph_dir), 
+        output_fname='full_batches_'+layer, 
+        is_parallel=False
+    )
